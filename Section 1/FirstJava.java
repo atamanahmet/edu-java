e3488d355e720505e304c736f0365a60689ae06f
atamanahmet <ahmtataman@proton.me>
2024-11-05 12:27:15 +0300

    new file:   .gitignore 	new file:   Section 1/FirstJava.class 	new file:   Section 1/FirstJava.java 	deleted:    Section 1/Hello.java

---
+++ Section 1/FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Print 1");
+        System.out.println("Java print");
+        System.out.println("Java 3 print");
+        System.out.println("Java print 4");
+    }
+}
