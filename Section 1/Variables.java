4121d5bfef5f5e410706a24075776e5ca0d8217c
atamanahmet <ahmtataman@proton.me>
2024-11-06 11:28:12 +0300

    modified:   Section 1/Variables.java

--- Section 1/Variables.java
+++ Section 1/Variables.java
@@ -1,8 +1,24 @@
 public class Variables {
     public static void main(String[] args) {
-        double a = 100;
-        int b = 30;
-        // double result = a / b;
-        System.out.println((a / b) * 30);
+        int numOfApples = 0;
+        int numOfCustomers = 0;
+        double profit = 0;
+        double price = 0.40;
+
+        System.out.println("Apple count: " + (numOfApples += 500));
+        System.out.println("bought1: " + (numOfApples -= 4));
+        numOfCustomers++;
+
+        profit += (4 * price);
+        System.out.println("bought2:" + (profit += (20 * price)));
+
+        numOfCustomers++;
+        numOfApples -= 20;
+        System.out.println("bought3: " + (profit += 200 * price));
+        numOfCustomers++;
+        numOfApples -= 200;
+
+        System.out.println("profit: " + profit + "$ remainder: " + numOfApples + " customer count : " + numOfCustomers);
+
     }
 }
