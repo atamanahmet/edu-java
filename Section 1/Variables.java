6a9036e9d3748f89611842db166b10ebab160705
atamanahmet <ahmtataman@proton.me>
2024-11-06 11:04:00 +0300

    modified:   Section 1/Variables.java

--- Section 1/Variables.java
+++ Section 1/Variables.java
@@ -1,17 +1,8 @@
 public class Variables {
     public static void main(String[] args) {
-        int points = 0;
-        points -= 50;
-        System.out.println(points);
-        points -= 3;
-        System.out.println(points);
-        points += 30;
-        System.out.println(points);
-        points += 100;
-        System.out.println(points);
-        points += 60;
-        System.out.println("Final points: " + points);
-        String string1 = "asdasd";
-        System.out.println(string1);
+        double a = 100;
+        int b = 30;
+        // double result = a / b;
+        System.out.println((a / b) * 30);
     }
 }
