b0c3f79854d95da0e5b09b58a3968ae11656f686
atamanahmet <ahmtataman@proton.me>
2024-11-07 13:14:35 +0300

    modified:   Section 1/Ifelseif.java

--- Section 1/Ifelseif.java
+++ Section 1/Ifelseif.java
@@ -1,16 +1,25 @@
 public class Ifelseif {
     public static void main(String[] args) {
-        int grif = 100;
-        int raven = 500;
+        // int grif = 100;
+        // int raven = 500;
 
-        if (grif - raven >= 300) {
-            System.out.println("Griffindor 1st");
-        } else if (grif >= raven && grif - raven < 300) {
-            System.out.println("Griffindor 2th");
-        } else if (raven - grif <= 100 && raven - grif >= 0) {
-            System.out.println("Griffindor 3th");
-        } else if (raven - grif > 100) {
-            System.out.println("Griffindor 4th");
+        // if (grif - raven >= 300) {
+        // System.out.println("Griffindor 1st");
+        // } else if (grif >= raven && grif - raven < 300) {
+        // System.out.println("Griffindor 2th");
+        // } else if (raven - grif <= 100 && raven - grif >= 0) {
+        // System.out.println("Griffindor 3th");
+        // } else if (raven - grif > 100) {
+        // System.out.println("Griffindor 4th");
+        // }
+        int temp = -2;
+        String forecast;
+        if (temp <= -1) {
+            System.out.println("Freezing!");
+        } else if (temp > -1 && temp <= 10) {
+            System.out.println("Chilly..");
+        } else {
+            System.out.println("Warm!");
         }
     }
 }
