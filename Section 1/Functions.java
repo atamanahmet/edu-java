3f9d1b223351716ba7099ab144a8ec238a4464f3
atamanahmet <ahmtataman@proton.me>
2024-11-07 15:01:19 +0300

    modified:   Section 1/Functions.java

--- Section 1/Functions.java
+++ Section 1/Functions.java
@@ -1,13 +1,18 @@
 public class Functions {
     public static void main(String[] args) {
-        printLines();
+        int a = 3;
+        // String b = "asdasd";
+        printLines(a, 3);
     }
 
-    public static void printLines() {
-        System.out.println("I will not copy and paste code.");
-        System.out.println("I will use Camel Case when writing class names.");
-        System.out.println("I will use lower Camel Case when writing function names.");
-        System.out.println("I will use lower Camel Case when writing variables names.");
+    public static void printLines(double a, double b) {
+        System.out.println("result : " + a * b);
+        // System.out.println("I will not copy and paste code.");
+        // System.out.println("I will use Camel Case when writing class names.");
+        // System.out.println("I will use lower Camel Case when writing function
+        // names.");
+        // System.out.println("I will use lower Camel Case when writing variables
+        // names.");
 
     }
 }
