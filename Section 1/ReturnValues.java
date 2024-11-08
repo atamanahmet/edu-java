2acd1a7a9b202797377582f00ef7e1ac3897c930
atamanahmet <ahmtataman@proton.me>
2024-11-08 11:43:12 +0300

    modified:   Section 1/ReturnValues.java

--- Section 1/ReturnValues.java
+++ Section 1/ReturnValues.java
@@ -1,5 +1,11 @@
 public class ReturnValues {
     public static void main(String[] args) {
+        double result = calculateArea(2, 1.8);
+        System.out.println(result);
+    }
 
+    public static double calculateArea(double width, double height) {
+        double area = width * height;
+        return area;
     }
 }
