8b4b6c137ac17a704f9353391da88112b82c8ee7
atamanahmet <ahmtataman@proton.me>
2024-11-12 11:25:01 +0300

    modified:   Section 1/BuiltInFunctions.java

--- Section 1/BuiltInFunctions.java
+++ Section 1/BuiltInFunctions.java
@@ -1,23 +1,26 @@
 
 public class BuiltInFunctions {
     public static void main(String[] args) {
-        String a = "Test 123 Test";
+        // String a = "Test 123 Test";
 
-        System.out.println(a.startsWith("t"));
-        String b = "Deneme Deneme";
-        b = b.replaceAll("e", "d");
-        System.out.println(b);
+        // System.out.println(a.startsWith("t"));
+        // String b = "Deneme Deneme";
+        // b = b.replaceAll("e", "d");
+        // System.out.println(b);
 
-        double c = 71;
-        double sqrtC = Math.sqrt(c);
-        System.out.println(sqrtC);
+        // double c = 71;
+        // double sqrtC = Math.sqrt(c);
+        // System.out.println(sqrtC);
 
-        double base = 2;
-        double exponent = 3;
-        double result = Math.pow(base, exponent);
-        System.out.println(result);
+        // double base = 2;
+        // double exponent = 3;
+        // double result = Math.pow(base, exponent);
+        // System.out.println(result);
 
-        double random = Math.random();
+        // between 0 and 100
+        double random = Math.floor((Math.random() * 100) + 1);
+
         System.out.println(random);
+
     }
 }
