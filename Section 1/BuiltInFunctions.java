940bd4b42e6549cb18faedc08fabac5255469091
atamanahmet <ahmtataman@proton.me>
2024-11-12 11:10:30 +0300

    modified:   Section 1/BuiltInFunctions.java

--- Section 1/BuiltInFunctions.java
+++ Section 1/BuiltInFunctions.java
@@ -3,7 +3,13 @@ public class BuiltInFunctions {
     public static void main(String[] args) {
         String a = "Test 123 Test";
 
-        System.out.println(a.startsWith("T"));
+        System.out.println(a.startsWith("t"));
+        String b = "Deneme Deneme";
+        b = b.replaceAll("e", "d");
+        System.out.println(b);
 
+        int c = 71;
+        double d = Math.sqrt(c);
+        System.out.println(d);
     }
 }
