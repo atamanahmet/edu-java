fdfaa6f496b7f22faca232d664c4f2c6a9f5392d
atamanahmet <ahmtataman@proton.me>
2024-11-12 12:17:53 +0300

    new file:   Section 1/WorkBook4_5.java

---
+++ Section 1/WorkBook4_5.java
@@ -0,0 +1,11 @@
+public class WorkBook4_5 {
+
+    public static void main(String[] args) {
+        System.out.println(areaSquare(5));
+    }
+
+    public static double areaSquare(double side) {
+        return Math.pow(side, 2);
+    }
+
+}
