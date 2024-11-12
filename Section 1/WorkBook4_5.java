373b6da24b44fb83c61f8b7d5951e92489b87b42
atamanahmet <ahmtataman@proton.me>
2024-11-12 13:11:03 +0300

    modified:   Section 1/WorkBook4_5.java

--- Section 1/WorkBook4_5.java
+++ Section 1/WorkBook4_5.java
@@ -1,11 +1,23 @@
 public class WorkBook4_5 {
 
     public static void main(String[] args) {
-        System.out.println(areaSquare(5));
+        System.out.println(areaSquare(3));
     }
 
     public static double areaSquare(double side) {
-        return Math.pow(side, 2);
+        if (side < 0) {
+            System.out.println("Calculation imposible");
+            System.exit(1);
+            return 0;
+        } else {
+
+            return Math.pow(side, 2);
+        }
+
+    }
+
+    public static double areaRectangle(double width, double height) {
+        return height * width;
     }
 
 }
