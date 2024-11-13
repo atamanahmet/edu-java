60d419a644b203fee8b3412842e0a69aeffb5ad7
atamanahmet <ahmtataman@proton.me>
2024-11-13 11:38:17 +0300

    modified:   Section 1/Loops.java

--- Section 1/Loops.java
+++ Section 1/Loops.java
@@ -3,8 +3,20 @@ public class Loops {
         // for (int i = 1; i <= 10; i++) {
         // System.out.println(i);
         // }
+
+        // String items = ["asd","bsd","zxc"];
         for (int i = 0; i < 10; i++) {
             System.out.println("5x" + i + " : " + 5 * i);
         }
+        // items.forEach(e){
+        // e
+        // }
+
+        for (int i = 1; i < 11; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
     }
 }
