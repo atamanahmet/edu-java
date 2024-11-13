e4b5b4bbccad1e98b7c63ccb23bc85b2cf496f21
atamanahmet <ahmtataman@proton.me>
2024-11-13 12:41:56 +0300

    modified:   Section 1/Loops.java

--- Section 1/Loops.java
+++ Section 1/Loops.java
@@ -26,22 +26,45 @@ public class Loops {
         // for (int i = 1; i < 11; i++) {
         // System.out.println(i + ". Test 123123123");
         // }
-        getSentence();
+        // getSentence();
+        // for (int i = 0; i < 10; i++) {
+        // System.out.print(i + " ");
+
+        // }
+        getNumbersAndWrite();
     }
 
-    public static void getSentence() {
+    public static void getNumbersAndWrite() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the sentence you want to write?");
-        String content = scan.nextLine();
-        System.out.println("How many time you want to write?");
-        int count = scan.nextInt();
-        writeSentence(content, count);
-        scan.close();
+        System.out.println("Number to count by: ");
+        int by = scan.nextInt();
+        System.out.println("Number to start from: ");
+        int from = scan.nextInt();
+        System.out.println("Number to count to: ");
+        int to = scan.nextInt();
+        writeNumbers(by, from, to);
+
     }
 
-    public static void writeSentence(String stringToWrite, int writeCount) {
-        for (int i = 1; i <= writeCount; i++) {
-            System.out.println(i + ". " + stringToWrite);
+    public static void writeNumbers(int byNumber, int fromNumber, int toNumber) {
+        for (int i = fromNumber; i <= toNumber; i += byNumber) {
+            System.out.print(fromNumber + " ");
         }
     }
+    // public static void getSentence() {
+    // Scanner scan = new Scanner(System.in);
+    // System.out.println("What is the sentence you want to write?");
+    // String content = scan.nextLine();
+    // System.out.println("How many time you want to write?");
+    // int count = scan.nextInt();
+    // writeSentence(content, count);
+    // scan.close();
+    // }
+
+    // public static void writeSentence(String stringToWrite, int writeCount) {
+    // for (int i = 1; i <= writeCount; i++) {
+    // System.out.println(i + ". " + stringToWrite);
+    // }
+    // }
+
 }
