ca2fec4c1f4c17d08a7a42df05ba315ff05ec160
atamanahmet <ahmtataman@proton.me>
2024-11-13 12:02:56 +0300

    modified:   Section 1/Loops.java

--- Section 1/Loops.java
+++ Section 1/Loops.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
         // for (int i = 1; i <= 10; i++) {
@@ -5,23 +7,40 @@ public class Loops {
         // }
 
         // String items = ["asd","bsd","zxc"];
-        for (int i = 0; i < 10; i++) {
-            System.out.println("5x" + i + " : " + 5 * i);
-        }
+        // for (int i = 0; i < 10; i++) {
+        // System.out.println("5x" + i + " : " + 5 * i);
+        // }
         // items.forEach(e){
         // e
         // }
-        int counter = 1;
-        for (int i = 1; i < 30; i++) {
-            if (counter != 11) {
-                if (i % 2 == 0) {
-                    System.out.println(i);
-                    System.out.println("Counter: " + counter);
-                    counter++;
-                }
-            }
+        // int counter = 1;
+        // for (int i = 1; i < 30; i++) {
+        // if (counter != 11) {
+        // if (i % 2 == 0) {
+        // System.out.println(i);
+        // System.out.println("Counter: " + counter);
+        // counter++;
+        // }
+        // }
+        // }
+        // for (int i = 1; i < 11; i++) {
+        // System.out.println(i + ". Test 123123123");
+        // }
+        getSentence();
+    }
 
+    public static void getSentence() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the sentence you want to write?");
+        String content = scan.nextLine();
+        System.out.println("How many time you want to write?");
+        int count = scan.nextInt();
+        writeSentence(content, count);
+    }
+
+    public static void writeSentence(String stringToWrite, int writeCount) {
+        for (int i = 1; i <= writeCount; i++) {
+            System.out.println(i + ". " + stringToWrite);
         }
-
     }
 }
