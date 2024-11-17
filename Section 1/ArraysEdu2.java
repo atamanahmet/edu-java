8f2620beabafe1f9fb7f9c435468f07487aeec5f
atamanahmet <ahmtataman@proton.me>
2024-11-17 13:40:55 +0300

    modified:   Section 1/ArraysEdu2.java

--- Section 1/ArraysEdu2.java
+++ Section 1/ArraysEdu2.java
@@ -4,27 +4,42 @@ public class ArraysEdu2 {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        scan.close();
+        double[] celciusArray = { 100, 0, 40, 27, 38, 20 };
+        double[] fahrenheitArray = {};
+        // getToppings();
+        celciusToFahrenheit(celciusArray, fahrenheitArray);
+        printArray(fahrenheitArray);
+        // scan.close();
     }
 
-    public static String[] afterTaxPriceCalc() {
-        String[] toppings = new String[3];
-        System.out.println("How many pizza toppings do you want?");
-        for (int i = 0; i < toppings.length; i++) {
-            System.out.print(i + ".");
-            toppings[i] = scan.nextLine();
+    // public static String[] getToppings() {
+    // String[] toppings = new String[3];
+    // System.out.println("How many pizza toppings do you want?");
+    // for (int i = 0; i < toppings.length; i++) {
+    // System.out.print(i + ".");
+    // toppings[i] = scan.nextLine();
+    // }
+    // System.out.println("Here's your toppings: ");
+    // for (int i = 0; i < toppings.length; i++) {
+    // toppings[i] = toppings[i].substring(0, 1).toUpperCase() +
+    // toppings[i].substring(1);
+    // System.out.println(i + ". " + toppings[i]);
+    // }
+    // return toppings;
+    // }
+
+    public static double[] celciusToFahrenheit(double[] fahrenheitArray, double[] celciusArray) {
+        for (int i = 0; i < celciusArray.length; i++) {
+            fahrenheitArray[i] = (celciusArray[i] / 5) + 32;
+            System.out.println(fahrenheitArray[i]);
         }
-        System.out.println("Here's your toppings: ");
-        for (int i = 0; i < toppings.length; i++) {
-            toppings[i] = toppings[i].substring(0, 1).toUpperCase() + toppings[i].substring(1);
-            System.out.println(i + ". " + toppings[i]);
-        }
-        return toppings;
+        return fahrenheitArray;
     }
 
-    public static double[] celcisuToFahrenheit() {
-
+    public static void printArray(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
 }
