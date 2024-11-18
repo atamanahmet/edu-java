e8d4da89231fd9da48aa2acc94c2ea1dd9d51134
atamanahmet <ahmtataman@proton.me>
2024-11-18 11:52:22 +0300

    modified:   Section 1/ArraysEdu3.java

--- Section 1/ArraysEdu3.java
+++ Section 1/ArraysEdu3.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 public class ArraysEdu3 {
     public static void main(String[] args) {
         int[] arr = { 10, 20, 39, 40, 50 };
+        // int[] newarr = arr;
+        // newarr[0] = 0;
+        // System.out.println(arr[0]);
+        // System.out.println(newarr[0]);
         int[] newarr = Arrays.copyOf(arr, arr.length);
         printArray(newarr);
     }
