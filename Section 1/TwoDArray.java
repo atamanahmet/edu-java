ce97aad1a846d12c364ee87c8e940b4777d67a9e
atamanahmet <ahmtataman@proton.me>
2024-11-18 12:23:48 +0300

    modified:   Section 1/TwoDArray.java

--- Section 1/TwoDArray.java
+++ Section 1/TwoDArray.java
@@ -3,12 +3,37 @@ public class TwoDArray {
         int[][] twoDArray = {
                 { 10, 20, 30, 40, 50 }, { 10, 20, 30, 40, 50 }, { 10, 20, 30, 40, 50 }, { 10, 20, 30, 40, 50 }
         };
+
+        // for (int i = 0; i < twoDArray.length; i++) {
+        // if (i == 0) {
+        // System.out.println("twoDArray: ");
+        // }
+        // System.out.print("{ ");
+        // for (int j = 0; j < twoDArray[i].length; j++) {
+        // System.out.print(twoDArray[i][j] + " ");
+        // }
+        // if (i == twoDArray.length - 1) {
+        // System.out.print("} ");
+        // } else {
+        // System.out.print("}, ");
+        // }
+        // }
         for (int i = 0; i < twoDArray.length; i++) {
-            System.out.print("{ ");
-            for (int j = 0; j < twoDArray[i].length; j++) {
-                System.out.print(twoDArray[i][j] + " ");
+            switch (i) {
+                case 0:
+                    for (int j = 0; j < twoDArray.length; j++) {
+                        switch (j) {
+                            case 0:
+                                System.out.print(twoDArray[i][j]);
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+
             }
-            System.out.print("}, ");
         }
+
     }
 }
