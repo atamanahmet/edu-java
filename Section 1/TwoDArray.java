b67296e7d26d79b78305b39f879870d9111d8a5e
atamanahmet <ahmtataman@proton.me>
2024-11-18 13:00:06 +0300

    modified:   Section 1/TwoDArray.java

--- Section 1/TwoDArray.java
+++ Section 1/TwoDArray.java
@@ -7,6 +7,7 @@ public class TwoDArray {
                 twoDArray[i][j] = getRandomNum();
             }
         }
+        print2DArray(twoDArray);
 
         // for (int i = 0; i < twoDArray.length; i++) {
         // if (i == 0) {
@@ -44,5 +45,28 @@ public class TwoDArray {
     public static int getRandomNum() {
         int randomNum = (int) (Math.random() * 100) + 1;
         return randomNum;
+    }
+
+    public static void print2DArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("{ ");
+
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+                if (j == arr[i].length - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(", ");
+                }
+
+            }
+            if (i == arr.length - 1) {
+                System.out.print("} ");
+            } else {
+                System.out.print("}, ");
+            }
+
+            System.out.println("");
+        }
     }
 }
