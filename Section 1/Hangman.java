e7a6f88bfde8ff5b513dc88cddfe16e12935086f
atamanahmet <ahmtataman@proton.me>
2024-11-22 15:48:06 +0300

    modified:   Section 1/Hangman.java

--- Section 1/Hangman.java
+++ Section 1/Hangman.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 // import java.lang.StringBuilder;
 
 public class Hangman {
+    public static int wrongCounter = 0;
     public static Scanner scan = new Scanner(System.in);
     public static String[] words = { "ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam",
             "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog",
@@ -68,6 +69,8 @@ public class Hangman {
     public static void main(String[] args) {
         boolean isWin = false;
         String word = randomWord(words);
+        boolean isTrue;
+        String prevGuesses = "";
         System.out.println(word);
         System.out.println(word.length());
         String hidden = "";
@@ -78,17 +81,38 @@ public class Hangman {
         }
         System.out.println(hidden);
         while (!isWin) {
-            System.out.println(gallows[0]);
+            if (wrongCounter == 6) {
+                System.out.println(gallows[wrongCounter]);
+                System.out.println("Game Over. You Hanged!");
+                System.exit(0);
+            }
+            isTrue = false;
+            System.out.println(gallows[wrongCounter]);
             System.out.println("Word: \t" + hidden);
+            System.out.println("\nMisses: " + prevGuesses);
             System.out.println(word);
             System.out.print("\nGuess: ");
             char guess = scan.nextLine().charAt(0);
             for (int i = 0; i < word.length(); i++) {
                 if (guess == word.charAt(i)) {
                     hidden = hidden.substring(0, i) + guess + hidden.substring(i + 1, word.length());
+                    isTrue = true;
                 }
             }
+            if (!isTrue) {
+                wrongCounter++;
+                prevGuesses += guess;
+            }
+            for (int i = 0; i < word.length(); i++) {
+                if (hidden.charAt(i) != word.charAt(i)) {
+                    isWin = false;
+                } else if (hidden.charAt(i) == word.charAt(i)) {
+                    isWin = true;
+                }
+            }
+
         }
+        System.out.println("You Win!");
 
     }
 
