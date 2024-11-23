2874a777e6e759c393967b216b2a274d36134dd9
atamanahmet <ahmtataman@proton.me>
2024-11-23 11:53:48 +0300

    new file:   Helsinki/Practice.java 	new file:   Section 1/Objects.java

---
+++ Helsinki/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    public static void main(String[] args) {
+        int dividend = 3;
+        int divisor = 2;
+
+        double result = dividend / (divisor * 1.0);
+        System.out.println(result);
+    }
+}
