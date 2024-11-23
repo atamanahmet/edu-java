285bdb9e728155700cc254069a53b53074872e7a
atamanahmet <ahmtataman@proton.me>
2024-11-23 11:59:30 +0300

    modified:   Helsinki/Practice.java

--- Helsinki/Practice.java
+++ Helsinki/Practice.java
@@ -1,9 +1,17 @@
 public class Practice {
     public static void main(String[] args) {
-        int dividend = 3;
-        int divisor = 2;
+        // int dividend = 3;
+        // int divisor = 2;
 
-        double result = dividend / (divisor * 1.0);
-        System.out.println(result);
+        // double result = dividend / (divisor * 1.0);
+        // System.out.println(result);
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
     }
 }
