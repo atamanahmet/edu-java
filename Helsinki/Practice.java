2e6af49d3d4160f7b2e8cdf158cafa4995c71011
atamanahmet <ahmtataman@proton.me>
2024-11-26 10:22:58 +0300

    modified:   Helsinki/Practice.java

--- Helsinki/Practice.java
+++ Helsinki/Practice.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
+
 public class Practice {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         // int dividend = 3;
         // int divisor = 2;
 
         // double result = dividend / (divisor * 1.0);
         // System.out.println(result);
-        System.out.println("Give the first number:");
-        int num1 = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int num2 = Integer.valueOf(scanner.nextLine());
-        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        // System.out.println("Give the first number:");
+        // int num1 = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give the second number:");
+        // int num2 = Integer.valueOf(scanner.nextLine());
+        // System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        // System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        // System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        // System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println("Give speed:");
+        int speed = Integer.valueOf(scanner.nextLine());
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
