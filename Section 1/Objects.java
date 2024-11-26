96b97bf68fdbef6673fa35a3670c05dcfe92708f
atamanahmet <ahmtataman@proton.me>
2024-11-26 11:03:55 +0300

    modified:   Section 1/Objects.java 	new file:   Section 1/Person.java

--- Section 1/Objects.java
+++ Section 1/Objects.java
@@ -1,14 +1,31 @@
+import java.util.Arrays;
+
 public class Objects {
     public static void main(String[] args) {
-        CarObject car1 = new CarObject();
-        CarObject car2 = new CarObject();
-        System.out.println(car1.brand);
-        car1.brand = "Opel";
-        System.out.println(car1.brand);
-        car2.brand = "Corsa";
-        car2.year = 2023;
-        // System.out.println(String.valueOf(car2));
-        System.out.println(car2.brand + " " + car2.year);
-        car1.drive();
+        // CarObject car1 = new CarObject();
+        // CarObject car2 = new CarObject();
+        // System.out.println(car1.brand);
+        // car1.brand = "Opel";
+        // System.out.println(car1.brand);
+        // car2.brand = "Corsa";
+        // car2.year = 2023;
+        // // System.out.println(String.valueOf(car2));
+        // System.out.println(car2.brand + " " + car2.year);
+        // car1.drive();
+        Person ataman = new Person();
+        ataman.name = "ahmet";
+        ataman.nationality = "Turkey";
+        ataman.dateOfBirth = "1992";
+        ataman.passport[0] = ataman.name;
+        ataman.passport[1] = ataman.nationality;
+        ataman.passport[2] = ataman.dateOfBirth;
+        ataman.seatNumber = 18;
+        // System.out.println(Arrays.toString(ataman.passport));
+        System.out.println(ataman.name);
+        System.out.println(ataman.nationality);
+        System.out.println(ataman.dateOfBirth);
+        System.out.println(Arrays.toString(ataman.passport));
+        System.out.println(ataman.seatNumber);
+
     }
 }
---
+++ Section 1/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    String nationality;
+    String dateOfBirth;
+    String[] passport = new String[] { "", "", "" };
+    int seatNumber;
+
+    // public Person(String[] infos) {
+    // this.passport = new String[3];
+    // }
+}
