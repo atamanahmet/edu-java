dd8e1cb549660bf88885a09773cfc74b38dcbc1d
atamanahmet <ahmtataman@proton.me>
2024-12-05 14:25:42 +0300

    modified:   Section 1/HelsinkiOBJ/Persontest.java

--- Section 1/HelsinkiOBJ/Persontest.java
+++ Section 1/HelsinkiOBJ/Persontest.java
@@ -255,3 +255,36 @@
 // return
 // this.squares*this.princePerSquare>compare.squares*compare.princePerSquare;
 // }
+
+// public class Book {
+
+// private String name;
+// private int publicationYear;
+
+// public Book(String name, int publicationYear) {
+// this.name = name;
+// this.publicationYear = publicationYear;
+// }
+
+// public String getName() {
+// return name;
+// }
+
+// public int getPublicationYear() {
+// return publicationYear;
+// }
+// public boolean equals(Object compare){
+// if(this==compare){
+// return true;
+// }
+// if(!(compare instanceof Book)){
+// return false;
+// }
+// Book temp = (Book)compare;
+// if(this.name.equals(temp.name)&&this.publicationYear==temp.publicationYear){
+// return true;
+// }
+// return false;
+// }
+
+// }
