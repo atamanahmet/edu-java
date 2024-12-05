2a55561fc2dd0065fd9c40ab5ed0c001bc1868e0
atamanahmet <ahmtataman@proton.me>
2024-12-05 13:35:38 +0300

    modified:   Section 1/HelsinkiOBJ/Persontest.java

--- Section 1/HelsinkiOBJ/Persontest.java
+++ Section 1/HelsinkiOBJ/Persontest.java
@@ -225,3 +225,29 @@
 // }
 
 // }
+
+// public class Apartment {
+
+// private int rooms;
+// private int squares;
+// private int princePerSquare;
+
+// public Apartment(int rooms, int squares, int pricePerSquare) {
+// this.rooms = rooms;
+// this.squares = squares;
+// this.princePerSquare = pricePerSquare;
+// }
+// public boolean largerThan(Apartment compared){
+// if(this.squares>compared.squares){
+// return true;
+// }
+// else{
+// return false;
+// }
+// }
+// public int priceDifference(Apartment compare){
+// return
+// Math.abs(this.squares*this.princePerSquare-compare.squares*compare.princePerSquare);
+// }
+
+// }
