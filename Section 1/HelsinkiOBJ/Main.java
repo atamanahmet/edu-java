33e77a4d21c8a83809a97312b044e864134075fb
atamanahmet <ahmtataman@proton.me>
2024-12-05 15:34:44 +0300

    deleted:    Section 1/HelsinkiOBJ/Book.java 	new file:   Section 1/HelsinkiOBJ/Main.java 	renamed:    Section 1/HelsinkiOBJ/Persontest.java -> Section 1/HelsinkiOBJ/SimpleDate.java

---
+++ Section 1/HelsinkiOBJ/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test your program here!
+        SimpleDate test = new SimpleDate(12, 12, 2000);
+        test.advance(10);
+        System.out.println(test);
+    }
+}
