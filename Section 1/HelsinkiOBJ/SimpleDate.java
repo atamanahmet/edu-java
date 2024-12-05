9dad728a027860b3ba3a7b5c8c2ad39af04f54a6
atamanahmet <ahmtataman@proton.me>
2024-12-05 16:16:45 +0300

    modified:   Section 1/HelsinkiOBJ/SimpleDate.java

--- Section 1/HelsinkiOBJ/SimpleDate.java
+++ Section 1/HelsinkiOBJ/SimpleDate.java
@@ -322,57 +322,57 @@
 // }
 // }
 
-public class SimpleDate {
+// public class SimpleDate {
 
-    private int day;
-    private int month;
-    private int year;
+// private int day;
+// private int month;
+// private int year;
 
-    public SimpleDate(int day, int month, int year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
+// public SimpleDate(int day, int month, int year) {
+// this.day = day;
+// this.month = month;
+// this.year = year;
+// }
 
-    @Override
-    public String toString() {
-        return this.day + "." + this.month + "." + this.year;
-    }
+// @Override
+// public String toString() {
+// return this.day + "." + this.month + "." + this.year;
+// }
 
-    public void advance(int howManyDays) {
-        if (this.day + howManyDays <= 30) {
-            this.day = this.day + howManyDays;
-        }
-        if (this.day + howManyDays > 30) {
+// public void advance(int howManyDays) {
+// if (this.day + howManyDays <= 30) {
+// this.day = this.day + howManyDays;
+// }
+// if (this.day + howManyDays > 30) {
 
-            this.day = this.day + howManyDays - 30;
+// this.day = this.day + howManyDays - 30;
 
-            if (this.month < 12) {
-                this.month = this.month + 1;
-            } else if (this.month == 12) {
-                this.month = 1;
-                this.year = this.year + 1;
-            }
+// if (this.month < 12) {
+// this.month = this.month + 1;
+// } else if (this.month == 12) {
+// this.month = 1;
+// this.year = this.year + 1;
+// }
 
-        }
+// }
 
-    }
+// }
 
-    public boolean before(SimpleDate compared) {
-        if (this.year < compared.year) {
-            return true;
-        }
+// public boolean before(SimpleDate compared) {
+// if (this.year < compared.year) {
+// return true;
+// }
 
-        if (this.year == compared.year && this.month < compared.month) {
-            return true;
-        }
+// if (this.year == compared.year && this.month < compared.month) {
+// return true;
+// }
 
-        if (this.year == compared.year && this.month == compared.month
-                && this.day < compared.day) {
-            return true;
-        }
+// if (this.year == compared.year && this.month == compared.month
+// && this.day < compared.day) {
+// return true;
+// }
 
-        return false;
-    }
+// return false;
+// }
 
-}
+// }
