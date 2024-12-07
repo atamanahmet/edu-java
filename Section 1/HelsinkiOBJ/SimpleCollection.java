5a8b3170936ce8557163c82a21dcaf6e33959a92
atamanahmet <ahmtataman@proton.me>
2024-12-07 11:46:41 +0300

    renamed:    Section 1/HelsinkiOBJ/Money.java -> Section 1/HelsinkiOBJ/SimpleCollection.java

--- Section 1/HelsinkiOBJ/SimpleCollection.java
+++ Section 1/HelsinkiOBJ/SimpleCollection.java
@@ -524,3 +524,36 @@
 // }
 // return output;
 // }
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+
+    public String longest(){
+        if(this.elements.isEmpty()){
+            return null;
+        }
+        for(int i =0; i<this.elements.size(); i++){
+            if(this.elements.get(0).name.length()<this.elements.get(i+1))
+        }
+        
+        return "";
+    }
+
+}
