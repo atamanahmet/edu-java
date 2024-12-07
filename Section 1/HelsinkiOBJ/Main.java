bf9269962623c6ac9cdd6ce8db3fb8728e28110d
atamanahmet <ahmtataman@proton.me>
2024-12-07 12:06:44 +0300

    modified:   Section 1/HelsinkiOBJ/Main.java 	deleted:    Section 1/HelsinkiOBJ/SimpleCollection.java

--- Section 1/HelsinkiOBJ/Main.java
+++ Section 1/HelsinkiOBJ/Main.java
@@ -51,19 +51,39 @@
 // }
 // }
 
+// public class Main {
+
+//     public static void main(String[] args) {
+
+//         // Try out your class here
+//         SimpleCollection c = new SimpleCollection("characters");
+//         System.out.println("Longest: " + c.longest());
+
+//         c.add("magneto");
+//         c.add("mystique");
+//         c.add("phoenix");
+
+//         System.out.println("Longest: " + c.longest());
+
+//     }
+// }
+
 public class Main {
 
     public static void main(String[] args) {
+        // Test your class here
+        Room room = new Room();
+        System.out.println("Empty room? " + room.isEmpty());
+        room.add(new Person("Lea", 183));
+        room.add(new Person("Kenya", 182));
+        room.add(new Person("Auli", 186));
+        room.add(new Person("Nina", 172));
+        room.add(new Person("Terhi", 185));
+        System.out.println("Empty room? " + room.isEmpty());
 
-        // Try out your class here
-        SimpleCollection c = new SimpleCollection("characters");
-        System.out.println("Longest: " + c.longest());
-
-        c.add("magneto");
-        c.add("mystique");
-        c.add("phoenix");
-
-        System.out.println("Longest: " + c.longest());
-
+        System.out.println("");
+        for (Person person : room.getPersons()) {
+            System.out.println(person);
+        }
     }
 }
