580e012dbced10003556332eb8c91fb11aea11d2
atamanahmet <ahmtataman@proton.me>
2024-12-09 12:09:54 +0300

    modified:   Section 1/H/Main.java 	deleted:    Section 1/H/SimpleDictionary.java 	deleted:    Section 1/H/TextUI.java 	new file:   Section 1/H/TodoList.java 	new file:   Section 1/H/UserInterface.java

---
+++ Section 1/H/TodoList.java
@@ -0,0 +1,83 @@
+
+// // import java.util.ArrayList;
+// import java.util.Scanner;
+
+// public class TextUI {
+
+//     private Scanner scanner;
+//     private SimpleDictionary simpleDictionary;
+
+//     public TextUI(Scanner scanner, SimpleDictionary simpleDictionary) {
+//         this.scanner = scanner;
+//         this.simpleDictionary = simpleDictionary;
+//     }
+
+//     public void start() {
+//         while (true) {
+//             System.out.print("Command: ");
+//             String input = scanner.nextLine();
+//             if (input.equals("end")) {
+//                 System.out.println("Bye Bye!");
+//                 break;
+//             }
+//             if (input.equals("add")) {
+
+//                 System.out.print("Word: ");
+
+//                 String word = scanner.nextLine();
+//                 System.out.print("Translation: ");
+//                 String translation = scanner.nextLine();
+//                 simpleDictionary.add(word, translation);
+
+//             } else if (input.equals("search")) {
+//                 System.out.print("To be translated: ");
+//                 String word = scanner.nextLine();
+//                 String translation = simpleDictionary.translate(word);
+//                 if (translation == null) {
+//                     System.out.println("Word (" + word + ") was not found");
+//                 } else {
+//                     System.out.println("Translation: " + translation);
+//                 }
+
+//             } else {
+//                 System.out.println("Unknown command");
+//             }
+
+//         }
+//     }
+// }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fox
+ */
+import java.util.ArrayList;
+
+public class TodoList {
+
+    private ArrayList<String> todoList;
+
+    public TodoList() {
+        this.todoList = new ArrayList<>();
+    }
+
+    public void add(String task) {
+        this.todoList.add(task);
+    }
+
+    public ArrayList<String> getTodoList() {
+        return this.todoList;
+    }
+
+    public void print() {
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println((i + 1) + ": " + todoList.get(i));
+        }
+    }
+
+}
---
+++ Section 1/H/UserInterface.java
@@ -0,0 +1,58 @@
+
+// import java.util.HashMap;
+
+// public class SimpleDictionary {
+
+// private HashMap<String, String> translations;
+
+// public SimpleDictionary() {
+// this.translations = new HashMap<>();
+// }
+
+// public String translate(String word) {
+// return this.translations.get(word);
+// }
+
+// public void add(String word, String translation) {
+// this.translations.put(word, translation);
+// }
+// }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fox
+ */
+// import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private Scanner scanner;
+    private TodoList list;
+
+    public UserInterface(TodoList list, Scanner scanner) {
+        this.list = list;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.print("Command: ");
+            String input = scanner.nextLine();
+            if (input.equals("stop")) {
+                break;
+            } else if (input.equals("add")) {
+                System.out.print("Task: ");
+                String task = scanner.nextLine();
+                list.add(task);
+            } else if (input.equals("list")) {
+                list.print();
+            }
+        }
+    }
+}
