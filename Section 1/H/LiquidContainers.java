3e685a1522990d82e1e565640acd5b852bf6d875
atamanahmet <ahmtataman@proton.me>
2024-12-11 10:40:33 +0300

    modified:   Section 1/H/LiquidContainers.java

--- Section 1/H/LiquidContainers.java
+++ Section 1/H/LiquidContainers.java
@@ -18,8 +18,36 @@ public class LiquidContainers {
             if (input.equals("quit")) {
                 break;
             }
-
+            String[] inputArray = new String[2];
+            inputArray = input.split(" ");
+            if (inputArray[0].equals("add")) {
+                add(first, Integer.valueOf(inputArray[1]));
+            } else if (inputArray[0].equals("move")) {
+                move(first, second, Integer.valueOf(inputArray[1]));
+            }
         }
     }
 
+    public static void add(int container, int liquid) {
+        if (container + liquid <= 100) {
+            container += liquid;
+        } else {
+            container = 100;
+        }
+    }
+
+    public static void move(int from, int to, int amount) {
+        if (to + amount <= 100) {
+            to += amount;
+            if (from - amount >= 0) {
+                from -= amount;
+            } else {
+                from = 0;
+            }
+
+        } else {
+            to = 100;
+        }
+
+    }
 }
