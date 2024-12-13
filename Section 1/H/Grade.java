f6f897d0e8de77009fe6ac140c2e06318f394512
atamanahmet <ahmtataman@proton.me>
2024-12-13 15:09:31 +0300

    new file:   Section 1/H/Grade.java

---
+++ Section 1/H/Grade.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fox
+ */
+import java.util.ArrayList;
+
+public class Grade {
+    private ArrayList<Integer> points = new ArrayList<>();
+    private ArrayList<Integer> grades = new ArrayList<>();
+
+    public Grade() {
+
+    }
+
+    public void addPoints(int point) {
+        if (point >= 0 || point <= 100) {
+            this.points.add(point);
+        }
+
+    }
+
+    public int sumOfPoints() {
+        int sum = 0;
+        for (int point : points) {
+            sum += point;
+        }
+        return sum;
+    }
+}
