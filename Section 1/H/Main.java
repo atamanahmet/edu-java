08e9b685f2629a2a0f327522d7354041d5d69a4c
atamanahmet <ahmtataman@proton.me>
2024-12-13 15:09:36 +0300

    new file:   Section 1/H/Main.java

---
+++ Section 1/H/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Grade grades = new Grade();
+        UserInterface userInterface = new UserInterface(scanner, grades);
+        // Write your program here -- consider breaking the program into
+        // multiple classes.
+    }
+}
