994b52e61126f88eda09a4da48932ae74bc65d38
atamanahmet <ahmtataman@proton.me>
2024-12-16 13:16:09 +0300

    new file:   Section 1/Big Year/Bird.java 	new file:   Section 1/Big Year/Main.java 	new file:   Section 1/Big Year/UserInterface.java

---
+++ Section 1/Big Year/Bird.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Bird {
+    private String name;
+    private String latinName;
+    private int observationCount;
+
+    public Bird(String name, String latinName) {
+        this.name = name;
+        this.latinName = latinName;
+        this.observationCount = 0;
+    }
+
+}
