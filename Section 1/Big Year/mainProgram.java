7be8014a28c39e56f760ea6686d799a1372e07be
atamanahmet <ahmtataman@proton.me>
2024-12-16 13:50:59 +0300

    modified:   Section 1/Big Year/mainProgram.java

--- Section 1/Big Year/mainProgram.java
+++ Section 1/Big Year/mainProgram.java
@@ -1,7 +1,17 @@
+
+import java.util.Scanner;
+
 public class mainProgram {
 
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
+        // NB! Do not create other scanner objects than the one below
+        // if and when you create other classes, pass the scanner to them
+        // as a parameter
+
+        Scanner scan = new Scanner(System.in);
+        UserInterface userInterface = new UserInterface(scan);
         userInterface.getInput();
+
     }
+
 }
