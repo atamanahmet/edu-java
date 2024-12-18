60309dfa9acfa777c0001b136bf5c54c1bafc6a3
atamanahmet <ahmtataman@proton.me>
2024-12-18 12:33:40 +0300

    new file:   Section 1/part8.2/Main.java 	new file:   Section 1/part8.2/Person.java

---
+++ Section 1/part8.2/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<Integer, Person> map = new HashMap<>();
+        ArrayList<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            persons.add(new Person(String.valueOf(i), i));
+        }
+        for (int j = 0; j < persons.size(); j++) {
+            for (Person person : persons) {
+                map.put(j, person);
+            }
+
+        }
+        for (int i = 0; i < persons.size(); i++) {
+            System.out.println(persons.get(i));
+            ;
+        }
+    }
+}
---
+++ Section 1/part8.2/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        return "Name: " + this.name + " / Age: " + this.age;
+    }
+}
