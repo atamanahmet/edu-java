5cc620164bb8b9ac22a5d5260b191af10c57e4f2
atamanahmet <ahmtataman@proton.me>
2024-12-19 14:19:29 +0300

    new file:   Section 1/part8.2/Book.java 	deleted:    Section 1/part8.2/Person.java 	modified:   Section 1/part8.2/Program.java

--- Section 1/part8.2/Program.java
+++ Section 1/part8.2/Program.java
@@ -1,77 +1,26 @@
-// // import java.util.HashMap;
-// // import java.util.ArrayList;
+import java.util.ArrayList;
 
-// // public class Main {
+public class Program {
 
-// // public static void main(String[] args) {
-// // HashMap<Integer, Person> map = new HashMap<>();
-// // ArrayList<Person> persons = new ArrayList<>();
-// // for (int i = 0; i < 100; i++) {
-// // persons.add(new Person(String.valueOf(i), i));
-// // }
-// // for (int j = 0; j < persons.size(); j++) {
+    public static void main(String[] args) {
+        ArrayList<Book> books = new ArrayList<>();
+        Book bookObject = new Book("Book Object", 2000, "...");
+        Book bookObject2 = new Book("Book Object", 2000, "...");
+        books.add(bookObject);
+        books.add(bookObject2);
+        int bookOne = books.get(0).getName().hashCode();
+        int bookTwo = books.get(1).getName().hashCode();
+        System.out.println(bookOne);
+        System.out.println(bookTwo);
 
-// // map.put(j, persons.get(j));
+        // if (books.contains(bookObject)) {
+        // System.out.println("Book Object was found.");
+        // }
 
-// // }
-// // // for (int i = 0; i < map.size(); i++) {
-// // // System.out.println(map.get(i));
-// // // }
-// // System.out.println(map);
-// // }
-// // }
+        // bookObject = new Book("Book Object", 2000, "...");
 
-// import java.util.HashMap;
-
-// public class Program {
-
-// public static void main(String[] args) {
-// // Test your program here!
-// HashMap<String, Book> hashmap = new HashMap<>();
-// hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
-// hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "...."));
-
-// printValues(hashmap);
-// System.out.println("---");
-// printValueIfNameContains(hashmap, "prejud");
-// }
-
-// public static void printValues(HashMap<String, Book> hashMap) {
-// for (Book book : hashMap.values()) {
-// System.out.println(book);
-// }
-// }
-
-// public static void printValueIfNameContains(HashMap<String, Book> hashMap,
-// String text) {
-// for (Book book : hashMap.values()) {
-// if (book.getName().contains(text)) {
-// System.out.println(book);
-// }
-// }
-// }
-
-// }
-
-// import java.util.HashMap;
-
-// public class IOU {
-
-// private HashMap<String, Double> hashMap;
-
-// public IOU() {
-// this.hashMap = new HashMap<>();
-// }
-
-// public void setSum(String toWhom, double amount) {
-// hashMap.put(toWhom, amount);
-// }
-
-// public double howMuchDoIOweTo(String toWhom) {
-// // if(hashMap.get(toWhom)!=null){
-// // return hashMap.get(toWhom);
-// // }
-// // return 0;
-// return hashMap.getOrDefault(toWhom, 0.0);
-// }
-// }
+        // if (!books.contains(bookObject)) {
+        // System.out.println("Book Object was not found.");
+        // }
+    }
+}
