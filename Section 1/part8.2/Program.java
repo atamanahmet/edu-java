564ba82c5fddeea1cbe5963cb24fe64137b49a38
atamanahmet <ahmtataman@proton.me>
2024-12-19 14:36:19 +0300

    modified:   Section 1/part8.2/Program.java

--- Section 1/part8.2/Program.java
+++ Section 1/part8.2/Program.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Program {
 
     public static void main(String[] args) {
         ArrayList<Book> books = new ArrayList<>();
+        HashMap<Book, String> borrowers = new HashMap<>();
         Book bookObject = new Book("Book Object", 2000, "...");
         Book bookObject2 = new Book("Book Object", 2000, "...");
         books.add(bookObject);
         books.add(bookObject2);
-        int bookOne = books.get(0).hashCode();
-        int bookTwo = books.get(1).hashCode();
+        borrowers.put(bookObject, "person1");
+        borrowers.put(bookObject2, "person1");
+
+        // int bookOne = books.get(0).hashCode();
+        // int bookTwo = books.get(1).hashCode();
+        int bookOne = borrowers.get(bookObject).hashCode();
+        int bookTwo = borrowers.get(bookObject2).hashCode();
 
         System.out.println(bookOne);
         System.out.println(bookTwo);
