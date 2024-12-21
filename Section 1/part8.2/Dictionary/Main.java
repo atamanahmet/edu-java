fef40e99817f8bb0df252cbf12194c71ded56c31
atamanahmet <ahmtataman@proton.me>
2024-12-21 12:36:01 +0300

    modified:   Section 1/part8.2/Dictionary/Main.java

--- Section 1/part8.2/Dictionary/Main.java
+++ Section 1/part8.2/Dictionary/Main.java
@@ -1,8 +1,12 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         // write your test code here
         // Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN",
@@ -21,11 +25,22 @@ public class Main {
         // i = i + 1;
         // }
         // System.out.println(ollie);
-        ArrayList<Person> persons = new ArrayList<Person>();
-        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+        // ArrayList<Person> persons = new ArrayList<Person>();
+        // persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN",
+        // 1200));
+        // persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-        printPersons(persons);
+        // printPersons(persons);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("item1");
+        list.add("item2");
+        ArrayList list2 = (ArrayList) list.clone();
+
+        list.remove(0);
+        System.out.println(list);
+        System.out.println(list2);
+        System.out.println(list2.getClass());
+        // System.out.println(list.getClass());
 
     }
 
