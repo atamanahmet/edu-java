cb488a6f881328717c9e700dfe59625724ce10cc
atamanahmet <ahmtataman@proton.me>
2024-12-22 12:23:02 +0300

    modified:   Section 1/part8.2/Dictionary/Main.java 	new file:   Section 1/part8.2/Dictionary/ProductWarehouse.java

---
+++ Section 1/part8.2/Dictionary/ProductWarehouse.java
@@ -0,0 +1,12 @@
+public class ProductWarehouse extends Warehouse {
+    private String productName;
+
+    public ProductWarehouse(String productName, double capacity) {
+        super(capacity);
+        this.productName = productName;
+    }
+
+    public String getName() {
+        return this.productName;
+    }
+}
