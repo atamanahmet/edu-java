0da7a5f3e9109a6e4964b35e53d1d3b89ec199b0
atamanahmet <ahmtataman@proton.me>
2024-12-23 12:30:45 +0300

    new file:   Section 1/part9last/BoxWithMaxWeight.java 	modified:   Section 1/part9last/Main.java

---
+++ Section 1/part9last/BoxWithMaxWeight.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class BoxWithMaxWeight extends Box {
+    private int capacity;
+    private int currentWeight;
+    private ArrayList<Item> itemList;
+
+    public BoxWithMaxWeight(int capacity) {
+        super();
+        this.itemList = new ArrayList<>();
+        this.capacity = capacity;
+        this.currentWeight = 0;
+    }
+
+    public void add(Item item) {
+        int itemWeight = item.getWeight();
+        if (itemWeight + this.currentWeight <= this.capacity) {
+            this.itemList.add(item);
+            this.currentWeight += itemWeight;
+        }
+    }
+
+    public boolean isInBox(Item item) {
+        if (this.itemList.contains(item)) {
+            return true;
+        }
+        return false;
+    }
+}
