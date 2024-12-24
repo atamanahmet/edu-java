7226b146a76691ca716b358aa695d2c99ce2d13c
atamanahmet <ahmtataman@proton.me>
2024-12-24 12:43:59 +0300

    modified:   Section 1/part9last/CustomTacoBox.java

--- Section 1/part9last/CustomTacoBox.java
+++ Section 1/part9last/CustomTacoBox.java
@@ -1,3 +1,15 @@
-public class CustomTacoBox {
+public class CustomTacoBox implements TacoBox {
+    private int tacos;
 
+    public CustomTacoBox(int tacos) {
+        this.tacos = tacos;
+    }
+
+    public void eat() {
+        this.tacos--;
+    }
+
+    public int tacosRemaining() {
+        return this.tacos;
+    }
 }
