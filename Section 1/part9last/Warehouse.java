be5f85ab5a0c06591a39ae6e4e49fdbb060082c5
atamanahmet <ahmtataman@proton.me>
2024-12-26 11:40:19 +0300

    modified:   Section 1/part9last/Warehouse.java 	modified:   Section 1/part9last/Main.java

--- Section 1/part9last/Warehouse.java
+++ Section 1/part9last/Warehouse.java
@@ -1,12 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Warehouse {
     private int price;
     private int stock;
     private String product;
+    private Map<String, Integer> productMap;
+
+    public Warehouse() {
+        this.productMap = new HashMap<>();
+    }
 
     public void addProduct(String product, int price, int stock) {
         this.product = product;
         this.price = price;
         this.stock = stock;
+        this.productMap.put(product, stock);
     }
 
     public int price(String product) {
