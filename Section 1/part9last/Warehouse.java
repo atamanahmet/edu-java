041379317ef654f7ac6993566dbbe39138dd91b7
atamanahmet <ahmtataman@proton.me>
2024-12-26 14:37:59 +0300

    modified:   Section 1/part9last/Warehouse.java

--- Section 1/part9last/Warehouse.java
+++ Section 1/part9last/Warehouse.java
@@ -3,46 +3,43 @@ import java.util.Map;
 import java.util.Set;
 
 public class Warehouse {
-    private int price;
-    private int stock;
-    private String product;
-    private Map<String, Integer> productMap;
+    private Map<String, Integer> productStock;
+    private Map<String, Integer> productPrice;
 
     public Warehouse() {
-        this.productMap = new HashMap<>();
+        this.productStock = new HashMap<>();
+        this.productPrice = new HashMap<>();
     }
 
     public void addProduct(String product, int price, int stock) {
         if (stock >= 0 || price >= 0) {
-            this.product = product;
-            this.price = price;
-            this.stock = stock;
-            this.productMap.put(product, stock);
+            this.productStock.put(product, stock);
+            this.productPrice.put(product, price);
         }
 
     }
 
     public int price(String product) {
-        if (this.productMap.containsKey(product)) {
-            return this.price;
+        if (this.productStock.containsKey(product)) {
+            return this.productPrice.get(product);
         } else {
             return -99;
         }
     }
 
     public int stock(String product) {
-        if (this.productMap.containsKey(product)) {
-            return this.productMap.get(product);
+        if (this.productStock.containsKey(product)) {
+            return this.productStock.get(product);
         } else {
             return 0;
         }
     }
 
     public boolean take(String product) {
-        if (this.productMap.containsKey(product) && (this.productMap.get(product) != 0)) {
-            int stock = this.productMap.get(product);
+        if (this.productStock.containsKey(product) && (this.productStock.get(product) != 0)) {
+            int stock = this.productStock.get(product);
             if (stock - 1 >= 0) {
-                this.productMap.replace(product, stock - 1);
+                this.productStock.replace(product, stock - 1);
                 return true;
             } else {
                 return false;
@@ -53,7 +50,7 @@ public class Warehouse {
     }
 
     public Set<String> products() {
-        Set<String> productNames = this.productMap.keySet();
+        Set<String> productNames = this.productStock.keySet();
         return productNames;
     }
 }
