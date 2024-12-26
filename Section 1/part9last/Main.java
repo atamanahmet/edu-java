d62ae4a03c7a842a2f7dd29449a1bd8b6a60939a
atamanahmet <ahmtataman@proton.me>
2024-12-26 14:20:03 +0300

    modified:   Section 1/part9last/Main.java 	modified:   Section 1/part9last/ShoppingCart.java 	new file:   Section 1/part9last/Store.java

--- Section 1/part9last/Main.java
+++ Section 1/part9last/Main.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ShoppingCart cart = new ShoppingCart();
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        // ShoppingCart cart = new ShoppingCart();
+        // cart.add("milk", 3);
+        // cart.print();
+        // System.out.println("cart price: " + cart.price() + "\n");
 
-        cart.add("buttermilk", 2);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        // cart.add("buttermilk", 2);
+        // cart.print();
+        // System.out.println("cart price: " + cart.price() + "\n");
 
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        // cart.add("milk", 3);
+        // cart.print();
+        // System.out.println("cart price: " + cart.price() + "\n");
 
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        // cart.add("milk", 3);
+        // cart.print();
+        // System.out.println("cart price: " + cart.price() + "\n");
+        // Scanner scanner = new Scanner(System.in);
+        // Warehouse warehose = new Warehouse();
+        // Store store = new Store(warehose, scanner);
+
+        // store.shop(null);
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProduct("coffee", 5, 10);
+        warehouse.addProduct("milk", 3, 20);
+        warehouse.addProduct("cream", 2, 55);
+        warehouse.addProduct("bread", 7, 8);
+
+        Scanner scanner = new Scanner(System.in);
+
+        Store store = new Store(warehouse, scanner);
+        store.shop("John");
     }
 }
--- Section 1/part9last/ShoppingCart.java
+++ Section 1/part9last/ShoppingCart.java
@@ -2,29 +2,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingCart {
-    private Map<String, Integer> cart;
+    private Map<String, Item> cart;
 
     public ShoppingCart() {
         this.cart = new HashMap<>();
     }
 
     public void add(String product, int price) {
-        if (this.cart.containsKey(product)) {
 
+        if (this.cart.containsKey(product)) {
+            this.cart.get(product).increaseQuantity();
+        } else {
+            Item item = new Item(product, 1, price);
+            this.cart.put(product, item);
         }
-        this.cart.put(product, price);
 
     }
 
     public int price() {
         int cartSum = 0;
-        for (int value : cart.values()) {
-            cartSum += value;
+        for (Item item : cart.values()) {
+            cartSum += item.price();
         }
         return cartSum;
     }
 
     public void print() {
-
+        for (Item item : this.cart.values()) {
+            System.out.println(item.toString());
+        }
     }
 }
