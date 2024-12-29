9c9e50b339c96c6f50d6dc1d27b622e3d2dc7669
atamanahmet <ahmtataman@proton.me>
2024-12-29 13:16:26 +0300

    modified:   Section 1/part9last/Aaa.java 	new file:   Section 1/part9last/Student.java

--- Section 1/part9last/Aaa.java
+++ Section 1/part9last/Aaa.java
@@ -4,16 +4,10 @@ import java.util.List;
 
 public class Aaa {
     public static void main(String[] args) {
-        List<Human> humans = new ArrayList<>();
-        humans.add(new Human("Merja", 500));
-        humans.add(new Human("Pertti", 80));
-        humans.add(new Human("Matti", 150000));
-        System.out.println(humans);
-        /*
-         * Uncomment the comment below when you have completed the compareTo-method.
-         */
-        Collections.sort(humans);
-        System.out.println(humans);
+
+        Student first = new Student("jamo");
+        Student second = new Student("jamo1");
+        System.out.println(first.compareTo(second));
     }
 
 }
