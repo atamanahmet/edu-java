11d97c1dc95f0cff564a5e5492ce41d97b1a6669
atamanahmet <ahmtataman@proton.me>
2024-12-29 13:31:42 +0300

    modified:   Section 1/part9last/Aaa.java 	new file:   Section 1/part9last/literacy.csv

--- Section 1/part9last/Aaa.java
+++ Section 1/part9last/Aaa.java
@@ -1,13 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Aaa {
     public static void main(String[] args) {
+        List<String[]> list = new ArrayList<>();
+        List<String[]> listBuffer = new ArrayList<>();
 
-        Student first = new Student("jamo");
-        Student second = new Student("jamo1");
-        System.out.println(first.compareTo(second));
+        try {
+            Files.lines(Paths.get("literacy.csv")).map(line -> line.split(","))
+                    .map(arr -> new String[] { arr[0], arr[1], arr[2] }).forEach(arr -> list.add(arr));
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        list.stream().forEach(arr -> System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]));
+
     }
 
 }
