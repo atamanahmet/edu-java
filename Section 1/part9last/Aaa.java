3dd0f3dc3788708d5945fcb991f68bb7739a87f8
atamanahmet <ahmtataman@proton.me>
2024-12-30 12:01:04 +0300

    modified:   Section 1/part9last/Aaa.java

--- Section 1/part9last/Aaa.java
+++ Section 1/part9last/Aaa.java
@@ -22,24 +22,37 @@ public class Aaa {
         // }
         // list.stream().forEach(arr -> System.out.println(arr[0] + ", " + arr[1] + ", "
         // + arr[2]));
-        Scanner scanner = new Scanner(System.in);
-        List<Book> books = new ArrayList<>();
-        while (true) {
-            System.out.print("Input the name of the book, empty stops: ");
-            String name = scanner.nextLine();
-            if (name.equals("")) {
-                break;
-            }
-            System.out.print("Input the age recommendation: ");
-            int ageReq = Integer.valueOf(scanner.nextLine());
-            books.add(new Book(name, ageReq));
-        }
-        Comparator<Book> comp = Comparator.comparing(Book::getAgeReq).thenComparing(Book::getName);
-        Collections.sort(books, comp);
-        System.out.println();
-        System.out.println(books.size() + " books in total.");
-        System.out.println();
-        books.stream().forEach(book -> System.out.println(book));
-    }
+        // Scanner scanner = new Scanner(System.in);
+        // List<String[]> books = new ArrayList<>();
+        // while (true) {
+        // System.out.print("Input the name of the book, empty stops: ");
+        // String name = scanner.nextLine();
+        // if (name.equals("")) {
+        // break;
+        // }
+        // System.out.print("Input the age recommendation: ");
+        // String ageReq = scanner.nextLine();
+        // books.add(new String[] { name, ageReq });
+        // }
+        // Comparator<String[]> comp =
+        // Comparator.comparing(String[]::String[].ageReq).thenComparing(Book::getName);
+        // Collections.sort(books, comp);
+        // System.out.println();
+        // System.out.println(books.size() + " books in total.");
+        // System.out.println();
+        // books.stream().forEach(book -> System.out.println(book));
 
+        // }
+
+        // public int getAgeReq(){
+        // return Integer.valueOf(this[1].);
+        // }
+        String name = "asdasd";
+        String name2 = "asdasd";
+        String name3 = "zxczxc";
+        name2 = "qweqwe";
+
+        System.out.println(name);
+
+    }
 }
