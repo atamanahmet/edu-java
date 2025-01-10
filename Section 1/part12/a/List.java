918a6cc7b6632876fe82f10cb64026b766388c64
atamanahmet <ahmtataman@proton.me>
2025-01-10 13:19:58 +0300

    modified:   Section 1/part12/a/List.java 	modified:   Section 1/part12/a/Main.java

--- Section 1/part12/a/List.java
+++ Section 1/part12/a/List.java
@@ -34,9 +34,14 @@ public class List<T> {
     public void grow() {
         int size = this.values.length;
         int newSize = size + (size / 2);
-        System.out.println("Size = " + size);
-        System.out.println("NewSize = " + newSize);
-        this.values = (T[]) Arrays.copyOf(this.values, newSize);
+        T[] bufferArray = (T[]) new Object[newSize];
+        // System.out.println("Size = " + size);
+        // System.out.println("NewSize = " + newSize);
+        // this.values = (T[]) Arrays.copyOf(this.values, newSize);
+        for (int i = 0; i < values.length; i++) {
+            bufferArray[i] = this.values[i];
+        }
+        this.values = bufferArray;
     }
 
     public int size() {
@@ -45,5 +50,31 @@ public class List<T> {
 
     public T get(int index) {
         return this.values[index];
+    }
+
+    public boolean contains(T value) {
+        for (int i = 0; i < this.firstEmptyIndex; i++) {
+            if (this.values[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove(T value) {
+        boolean isDone = false;
+        for (int i = 0; i < this.firstEmptyIndex; i++) {
+            if (this.values[i] == value || this.values[i].equals(value)) {
+                this.values[i] = null;
+                isDone = true;
+                for (int j = i; j < this.firstEmptyIndex; j++) {
+                    this.values[j] = this.values[j + 1];
+
+                }
+            }
+            if (isDone) {
+                break;
+            }
+        }
     }
 }
--- Section 1/part12/a/Main.java
+++ Section 1/part12/a/Main.java
@@ -9,17 +9,22 @@ public class Main {
     // List<String> list2 = new ArrayList<>();
     public static void main(String[] args) {
         List<String> myList = new List<>();
-        // myList.add("hello");
-        // myList.add("world");
-        for (int i = 0; i < 100; i++) {
-            try {
-                myList.add("hello" + i);
-            } catch (ClassCastException e) {
-                myList.add("hello" + i);
-            }
+        // // myList.add("hello");
+        // // myList.add("world");
+        for (int i = 0; i < 25; i++) {
+            myList.add("hello" + i);
+
         }
         for (int i = 0; i < myList.size(); i++) {
-            System.out.println(myList.get(i));
+            System.out.println(i + " " + myList.get(i));
         }
+        myList.remove("hello3");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(i + " " + myList.get(i));
+        }
+        // List<String> myList = new List<>();
+        // System.out.println(myList.contains("hello"));
+        // myList.add("hello");
+        // System.out.println(myList.contains("hello"));
     }
 }
