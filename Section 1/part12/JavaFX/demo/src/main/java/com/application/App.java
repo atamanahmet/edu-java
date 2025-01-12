8b9c1344e343a7d2cd6044ea6013953c2307ebe2
atamanahmet <ahmtataman@proton.me>
2025-01-12 23:37:01 +0300

    modified:   Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java 	modified:   Section 1/part12/JavaFX/demo/target/classes/com/application/App.class 	new file:   Section 1/part12/test/Test.java

--- Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java
+++ Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java
@@ -1,35 +1,61 @@
 package com.application;
 
+// import javafx.application.Application;
+// import javafx.stage.Stage;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.control.Label;
+// import javafx.scene.layout.BorderPane;
+// import javafx.scene.layout.FlowPane;
+
+// public class App extends Application {
+//     public static void main(String[] args) {
+
+//         // System.out.println(componentGroup.getChildren());
+//         launch(App.class);
+//     }
+
+//     public void start(Stage window) {
+//         Label text = new Label();
+//         text.setText("Diagonally");
+//         Button button = new Button("Button");
+//         BorderPane componentGroup = new BorderPane();
+//         componentGroup.setTop(new Button("Top"));
+//         componentGroup.setBottom(new Button("Bottom"));
+//         componentGroup.setRight(new Button("Right"));
+//         componentGroup.setLeft(new Button("Left"));
+//         // componentGroup.getChildren().add(text);
+
+//         Scene scene1 = new Scene(componentGroup);
+//         // window.setTitle("Test window");
+//         window.setScene(scene1);
+//         window.show();
+//     }
+
+// }
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) {
-
-        // System.out.println(componentGroup.getChildren());
         launch(App.class);
     }
 
     public void start(Stage window) {
-        Label text = new Label();
-        text.setText("Diagonally");
-        Button button = new Button("Button");
-        BorderPane componentGroup = new BorderPane();
-        componentGroup.setTop(new Button("Top"));
-        componentGroup.setBottom(new Button("Bottom"));
-        componentGroup.setRight(new Button("Right"));
-        componentGroup.setLeft(new Button("Left"));
-        // componentGroup.getChildren().add(text);
+        Label north = new Label("NORTH");
+        Label south = new Label("SOUTH");
+        Label east = new Label("EAST");
 
-        Scene scene1 = new Scene(componentGroup);
-        // window.setTitle("Test window");
-        window.setScene(scene1);
+        BorderPane layout = new BorderPane();
+        layout.setTop(north);
+        layout.setRight(east);
+        layout.setBottom(south);
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
         window.show();
     }
-
 }
