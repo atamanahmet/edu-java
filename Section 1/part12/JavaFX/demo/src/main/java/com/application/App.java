282f54d49a7cd69cbbd4362c892bf16df9e24cc7
atamanahmet <ahmtataman@proton.me>
2025-01-13 12:58:05 +0300

    modified:   Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java 	modified:   Section 1/part12/JavaFX/demo/src/main/java/module-info.java 	new file:   Section 1/part12/JavaFX/demo/target/classes/com/application/App$1.class 	modified:   Section 1/part12/JavaFX/demo/target/classes/com/application/App.class 	modified:   Section 1/part12/JavaFX/demo/target/classes/module-info.class 	new file:   Section 1/part12/test.txt

--- Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java
+++ Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java
@@ -1,65 +1,74 @@
 package com.application;
 
-// import javafx.application.Application;
-// import javafx.stage.Stage;
-// import javafx.scene.Scene;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.Label;
-// import javafx.scene.layout.BorderPane;
-// import javafx.scene.layout.FlowPane;
-
-// public class App extends Application {
-//     public static void main(String[] args) {
-
-//         // System.out.println(componentGroup.getChildren());
-//         launch(App.class);
-//     }
-
-//     public void start(Stage window) {
-//         Label text = new Label();
-//         text.setText("Diagonally");
-//         Button button = new Button("Button");
-//         BorderPane componentGroup = new BorderPane();
-//         componentGroup.setTop(new Button("Top"));
-//         componentGroup.setBottom(new Button("Bottom"));
-//         componentGroup.setRight(new Button("Right"));
-//         componentGroup.setLeft(new Button("Left"));
-//         // componentGroup.getChildren().add(text);
-
-//         Scene scene1 = new Scene(componentGroup);
-//         // window.setTitle("Test window");
-//         window.setScene(scene1);
-//         window.show();
-//     }
-
-// }
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
     public static void main(String[] args) {
         launch(App.class);
     }
 
     public void start(Stage window) {
-        Label north = new Label("NORTH");
-        Label south = new Label("SOUTH");
-        Label east = new Label("EAST");
+        int letters = 0;
+        int words = 0;
+        String longestWord = "";
+        BorderPane borderPane = new BorderPane();
+        Button button = new Button("Click");
 
-        VBox layout = new VBox(10);
-        layout.setSpacing(20);
-        layout.getChildren().add(north);
-        layout.getChildren().add(south);
-        layout.getChildren().add(east);
-        // layout.set
-        Scene scene = new Scene(layout);
+        Label text1 = new Label("Letters: " + letters);
+        Label text2 = new Label("Words: " + words);
+        Label text3 = new Label("The longest word is: " + longestWord);
+        TextArea textArea = new TextArea();
+        StringBuilder line = new StringBuilder("");
+
+        // button.setOnAction((event) -> System.out.println(textArea.getParagraphs()));
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                for (int i = 0; i < textArea.getParagraphs().size(); i++) {
+                    line.append(textArea.getParagraphs().get(i) + "\n");
+                }
+                System.out.println(textArea.getParagraphs());
+                System.out.println(line);
+                try {
+                    FileWriter writer = new FileWriter("test.txt");
+                    writer.write(String.valueOf(line));
+                    writer.close();
+
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+        });
+
+        // button.setOnAction((event) -> System.out.println(lines));
+        HBox hbox = new HBox();
+        hbox.getChildren().add(text1);
+        hbox.getChildren().add(text2);
+        hbox.getChildren().add(text3);
+        hbox.setSpacing(10);
+
+        borderPane.setCenter(textArea);
+        borderPane.setRight(button);
+
+        borderPane.setBottom(hbox);
+        Scene scene = new Scene(borderPane);
         window.setScene(scene);
         window.show();
+
     }
 }
--- Section 1/part12/JavaFX/demo/src/main/java/module-info.java
+++ Section 1/part12/JavaFX/demo/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.application {
     requires transitive javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.base;
 
     opens com.application to javafx.fxml;
 
