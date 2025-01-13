ca419ad68247d832ad1caad03d0f6549e0cb791e
atamanahmet <ahmtataman@proton.me>
2025-01-13 14:02:23 +0300

    modified:   Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java 	modified:   Section 1/part12/JavaFX/demo/target/classes/com/application/App$1.class 	modified:   Section 1/part12/JavaFX/demo/target/classes/com/application/App.class 	modified:   Section 1/part12/test.txt

--- Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java
+++ Section 1/part12/JavaFX/demo/src/main/java/com/application/App.java
@@ -2,6 +2,8 @@ package com.application;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.collections.ObservableList;
@@ -13,25 +15,30 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    Font font = new Font("Calibri", 50);
+    int letters = 0;
+    int words = 0;
+    String longestWord = "";
 
     public static void main(String[] args) {
         launch(App.class);
     }
 
     public void start(Stage window) {
-        int letters = 0;
-        int words = 0;
-        String longestWord = "";
+
         BorderPane borderPane = new BorderPane();
         Button button = new Button("Click");
 
-        Label text1 = new Label("Letters: " + letters);
-        Label text2 = new Label("Words: " + words);
-        Label text3 = new Label("The longest word is: " + longestWord);
+        Label text1 = new Label("Letters: ");
+        Label text2 = new Label("Words: ");
+        Label text3 = new Label("The longest word is: ");
         TextArea textArea = new TextArea();
+        textArea.setFont(font);
+        textArea.setWrapText(true);
         StringBuilder line = new StringBuilder("");
 
         // button.setOnAction((event) -> System.out.println(textArea.getParagraphs()));
@@ -40,11 +47,39 @@ public class App extends Application {
             public void handle(ActionEvent event) {
                 for (int i = 0; i < textArea.getParagraphs().size(); i++) {
                     line.append(textArea.getParagraphs().get(i) + "\n");
+                    String[] wordList = String.valueOf(line).split("\n");
+                    if (String.valueOf(wordList[0]) != null) {
+                        longestWord = String.valueOf(wordList[0]);
+                    } else {
+                        longestWord = " ";
+                    }
+                    text3.setText("The longest word is: ");
+                    for (int j = 0; j < wordList.length; j++) {
+                        if (longestWord.length() < wordList[j].length()) {
+                            longestWord = wordList[j];
+                        }
+                    }
+                    text1.setText("Words: ");
+                    for (int j = 0; j < textArea.getParagraphs().size(); j++) {
+                        if (wordList[j] != null || !wordList[j].equals("")) {
+                            words++;
+                        }
+                    }
+                    letters = 0;
+                    text1.setText("Letters: ");
+                    for (int j = 0; j < textArea.getParagraphs().size(); j++) {
+                        System.out.println("wordListLength" + wordList.length);
+                        wordList[j].chars().forEach(letter -> letters++);
+                        System.out.println(letters);
+                    }
                 }
-                System.out.println(textArea.getParagraphs());
-                System.out.println(line);
+                text1.setText(text1.getText() + letters);
+                text3.setText(text3.getText() + longestWord);
+                text2.setText(text2.getText() + words);
+                System.out.println("paragraph Count" + textArea.getParagraphs().size());
+                // System.out.println(longestWord);
                 try {
-                    FileWriter writer = new FileWriter("test.txt");
+                    FileWriter writer = new FileWriter("test.txt", StandardCharsets.UTF_8);
                     writer.write(String.valueOf(line));
                     writer.close();
 
@@ -71,4 +106,7 @@ public class App extends Application {
         window.show();
 
     }
+    // public String[] getWords(StringBuilder line){
+
+    // }
 }
