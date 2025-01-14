a8e27e11a5974022ec77506a89d64b7ddecefbfa
atamanahmet <ahmtataman@proton.me>
2025-01-14 12:41:32 +0300

    modified:   Section 1/part12/Event/event/src/main/java/com/event/App.java 	new file:   Section 1/part12/Event/event/src/main/java/com/event/Main.java 	modified:   Section 1/part12/Event/event/target/classes/com/event/App.class

--- Section 1/part12/Event/event/src/main/java/com/event/App.java
+++ Section 1/part12/Event/event/src/main/java/com/event/App.java
@@ -1,11 +1,14 @@
 package com.event;
 
-import java.util.Scanner;
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,14 +17,41 @@ public class App extends Application {
     }
 
     public void start(Stage window) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Give a title: ");
-        String title = scanner.nextLine();
+        BorderPane borderpane = new BorderPane();
+        VBox vbox = new VBox();
+        GridPane grid = new GridPane();
+        Label first = new Label("First View!");
+        Label second = new Label("Second View!");
+        Label third = new Label("Third View!");
+        Button buttonSecond = new Button("To the third view!");
+        Button buttonThird = new Button("To the first view!");
+        Button buttonFirst = new Button("To the second view!");
 
-        window.setTitle(title);
-        FlowPane layout = new FlowPane();
-        Scene scene = new Scene(layout);
-        window.setScene(scene);
+        borderpane.setTop(first);
+        borderpane.setCenter(buttonFirst);
+
+        vbox.getChildren().addAll(second, buttonSecond);
+
+        grid.add(third, 0, 0);
+        grid.add(buttonThird, 1, 1);
+
+        Scene sceneBorderPane = new Scene(borderpane);
+        Scene sceneVbox = new Scene(vbox);
+        Scene sceneGrid = new Scene(grid);
+
+        buttonFirst.setOnAction((event) -> {
+            window.setScene(sceneVbox);
+        });
+        buttonSecond.setOnAction((event) -> {
+            window.setScene(sceneGrid);
+        });
+        buttonThird.setOnAction((event) -> {
+            window.setScene(sceneBorderPane);
+        });
+
+        window.setScene(sceneBorderPane);
+
         window.show();
+
     }
 }
---
+++ Section 1/part12/Event/event/src/main/java/com/event/Main.java
@@ -0,0 +1,14 @@
+// package com.event;
+
+// import java.util.Scanner;
+
+// import javafx.application.Application;
+
+// public class Main {
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// System.out.print("Give a title: ");
+// String title = scanner.nextLine();
+// Application.launch(App.class, "--keyTitle=" + title);
+// }
+// }
